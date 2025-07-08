d7c75ce61ca8f3d4c86ba2a187cdaa2ec1b3865a
chauid <dlfdl28@naver.com>
2025-07-08 11:12:36 +0900

    [0.0.1] 배포

---
+++ src/main/java/com/postsmith/api/content/PostsmithApiContentApplication.java
@@ -0,0 +1,13 @@
+package com.postsmith.api.content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsmithApiContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsmithApiContentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postsmith/api/content/PostsmithApiContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postsmith.api.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsmithApiContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
